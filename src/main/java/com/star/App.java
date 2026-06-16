8bebfcc128c373ad6bb483c7d7bae54edd06c5c2
Hardi <hardivirani2003@gmail.com>
2026-06-15 20:28:59 -0400

    Vid-66: Running tomcat in ecplise (but ours is intellij)

--- src/main/java/com/star/App.java
+++ src/main/java/com/star/App.java
@@ -17,4 +17,3 @@ public class App
 
 // If you have ultimate, just do all this update of the files. -> run -> edit configuration -> + -> tomcat sever -> local -> application server -> choose root folder of apahe tomcat -> configure and you will be able to see the url below -> next tab beside server tab -> deployment -> + -> artifact -> i chose springMVC: war exploded (choose whatever suits your environment, ask AI about that) -> below that tab you will be able to see applicaition context ->  / springMVC -> just keep / or any other link. -> apply -> run your application.
 
-//
