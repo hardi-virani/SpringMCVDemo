325bbdbc2c911b3a13f9c9e118a29df33adf170a
Hardi <hardivirani2003@gmail.com>
2026-06-15 22:09:10 -0400

    vid 68 & 69 : configuring dispacther servlet & Internal Resource view Resolver.
    Comments added

--- src/main/java/com/star/HomeController.java
+++ src/main/java/com/star/HomeController.java
@@ -11,6 +11,11 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HomeController {
 
+    @ModelAttribute("course")
+    public String courseName() {
+        return "java";
+    }
+
     @RequestMapping("/home")
     public ModelAndView home(ModelAndView mv) {
         mv.setViewName("index");
--- src/main/webapp/WEB-INF/views/result.jsp
+++ src/main/webapp/WEB-INF/views/result.jsp
@@ -14,6 +14,7 @@
 
 <h2> Welcome to star world </h2> <br>
 <p> ${alien}</p>
+<p>Welcome to ${course} world</p>
 
 
 
